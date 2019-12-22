9c7545b76e863272a5670761c3677f63a3b2e6f6
Akram Khalid <akramkhalid12@gmail.com>
2019-12-21 22:56:38 -0600

    init commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,34 @@
-import React, { Component } from 'react'
+import React from "react";
+import VideoThumbnail from "reactautoplay";
 
-import ExampleComponent from 'reactautoplay'
+const App = () => {
 
-export default class App extends Component {
-  render () {
-    return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
+  return (
+    <div className="example">
+      <div className="flex">
+        <VideoThumbnail /> 
+
+        <VideoThumbnail
+          title='The Bachelor Premieres January 6'
+          preview='https://media1.giphy.com/media/llsb2HU9sKfK57pcnf/giphy480p.mp4'
+          badge='Premiere'
+          badgeBG='#EF798A'
+          message='1.2M Watching Now 🔥🔥'
+          muted={true}
+          width={350} 
+          classname="customClassName" /> 
+          
+          <VideoThumbnail 
+            title='Boogie With A Bear | Planet Earth II'
+            preview='https://media1.giphy.com/media/mFvxw25Jxy4TsHfjQE/giphy480p.mp4'
+            badge='Live'
+            message='24k Watching Now'  
+            /> 
       </div>
-    )
-  }
+    </div>
+  );
 }
+
+
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,22 +1,47 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-
 import styles from './styles.css'
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+const VideoThumbnail = ({ title, preview, badge, badgeBG, message, muted, width, classname }) => {
+  const onHover = (e) => {
+    e.target.play()
+    e.target.nextElementSibling.style.opacity = 0.4
   }
-
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
+  const offHover = (e) => {
+    e.target.pause()
+    e.target.nextElementSibling.style.opacity = 1
+  }
+  return (
+    <div style={{width: width}} className={`${styles.videoThumbnail} ${classname}`}>
+      <div style={{width: width}} className={styles.video}>
+        <video onMouseEnter={(e) => onHover(e)} onMouseOut={(e) => offHover(e)} className={styles.preview} width={width} src={preview} loop muted={muted}/>
+        <div className={styles.badge} style={{background: badgeBG}}>{badge}</div>
       </div>
-    )
-  }
+      <h4 className={styles.title}>{title}</h4>
+      <span className={styles.views}>{message}</span>
+    </div>
+  )
 }
+
+VideoThumbnail.propTypes = {
+  preview: PropTypes.string,
+  title: PropTypes.string,
+  message: PropTypes.string,
+  badge: PropTypes.string,
+  badgeBG: PropTypes.string,
+  muted: PropTypes.bool,
+  width: PropTypes.number,
+  classname: PropTypes.string
+}
+
+VideoThumbnail.defaultProps = {
+  preview: `https://media2.giphy.com/media/JrY9JfnL4OnXxnCi4F/giphy480p.mp4`,
+  title: `The Invisible Man - Official Trailer [HD]`,
+  message: `3.2k Watching Now`,
+  badge: `Live`,
+  muted: false,
+  width: 350,
+  classname: ''
+}
+
+export default VideoThumbnail
